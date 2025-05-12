af6207d6fb0ec5b2f247c73a53d0321c97cbb3f0
anaszmedina <103692416+anaszmedina@users.noreply.github.com>
2025-05-11 23:49:59 -0400

    reinserting hello.c content

---
+++ hello.c
@@ -0,0 +1,118 @@
+*
+ * Stephen A. Edwards
+ * Columbia University
+ */
+
+//testing push
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <math.h>
+#include "vga_ball.h"
+
+/*
+// Read and print the background color 
+void print_background_color() {
+  vga_ball_arg_t vla;
+  
+  if (ioctl(vga_ball_fd, VGA_BALL_READ_BACKGROUND, &vla)) {
+      perror("ioctl(VGA_BALL_READ_BACKGROUND) failed");
+      return;
+  }
+  printf("%02x %02x %02x\n",
+	 vla.background.red, vla.background.green, vla.background.blue);
+}
+
+// Set the background color 
+void set_background_color(const vga_ball_color_t *c)
+{
+  vga_ball_arg_t vla;
+  vla.background = *c;
+  if (ioctl(vga_ball_fd, VGA_BALL_WRITE_BACKGROUND, &vla)) {
+      perror("ioctl(VGA_BALL_SET_BACKGROUND) failed");
+      return;
+  }
+}
+*/
+
+//#define SCREEN_WIDTH 640
+//#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 578
+#define SCREEN_HEIGHT 449
+#define BALL_SPEED 3
+#define SLEEP_TIME 50000 //50ms delay between updates
+
+//RADIUS IS 31
+
+int main()
+{
+    int vga_ball_fd;
+    vga_ball_line_t vla_line;
+    static const char filename[] = "/dev/vga_ball";
+
+    // Ball position and velocity
+
+    float theta = 1;
+    int delta = 5;
+
+
+    int LineMatrix[256][2];
+    int i;
+    for (i = 0; i < 256; i++) {
+        LineMatrix[i][0] = 240;
+        LineMatrix[i][1] = 240;
+    }
+
+    printf("VGA ball Userspace program started\n");
+
+    if ((vga_ball_fd = open(filename, O_RDWR)) == -1) {
+        fprintf(stderr, "could not open %s\n", filename);
+        return -1;
+    }
+
+    // Main animation loop
+    while (1) {
+        // Update position
+        if (theta >= 178) {
+            theta = 1;
+        } else {
+            theta += 1;
+        }
+        
+        int max_y = (int) (256 * sin(theta * 3.14159265 / 180.0));
+
+        int y;
+        for (y = 0; y < 640; y++) {
+            if (y < max_y) {
+                // cosine of theta times the number of x values we have, times the x value we are on.
+                int virtual_x = ((float) (float)256/(float)(max_y)) * y;
+
+                LineMatrix[y][0] = 240 + (int) (((float) (256 * cos(theta * 3.14159265 / 180.0))) * virtual_x) - delta;
+                LineMatrix[y][1] = 240 + (int) (((float) (256 * cos(theta * 3.14159265 / 180.0))) * virtual_x) + delta;
+            } else {
+                LineMatrix[y][0] = -1;
+                LineMatrix[y][1] = -1;
+            }
+        }
+
+        memcpy(vla_line.LineMatrix, LineMatrix, sizeof(LineMatrix));
+
+
+        if (ioctl(vga_ball_fd, VGA_BALL_WRITE_LINE, &vla_line)) {
+            perror("ioctl(VGA_BALL_WRITE_LINE) failed");
+            break;
+        }
+
+        // Small delay to control animation speed
+        usleep(SLEEP_TIME);
+    }
+
+    printf("VGA ball userspace program terminating\n");
+    close(vga_ball_fd);
+    return 0;
+}
