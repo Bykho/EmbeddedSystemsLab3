7d2238bbd0a4e3499d32746b065fc136c9bb7c5c
Nico Bykhovsky <nico@bykhovsky.com>
2025-05-12 00:54:10 -0400

    s

--- hello.c
+++ hello.c
@@ -91,36 +91,31 @@ int main()
         }
         
         int max_y = (int) (256 * sin(theta * 3.14159265 / 180.0));
-        printf("DEBUG: max_y calculated: %d\n", max_y);
 
         int y;
         for (y = 0; y < 480; y++) {
             if (y < max_y) {
                 // cosine of theta times the number of x values we have, times the x value we are on.
                 int virtual_x = ((float) (float)256/(float)(max_y)) * y;
-                printf("DEBUG: Processing y=%d, virtual_x=%d\n", y, virtual_x);
 
                 LineMatrix[y][0] = 320 + (int) (((float) (256 * cos(theta * 3.14159265 / 180.0))) * virtual_x) - delta;
                 LineMatrix[y][1] = 320 + (int) (((float) (256 * cos(theta * 3.14159265 / 180.0))) * virtual_x) + delta;
-                
-                printf("DEBUG: LineMatrix[%d] values: [%d, %d]\n", y, LineMatrix[y][0], LineMatrix[y][1]);
+                if (y % 10 == 0) {
+                    printf("DEBUG: LineMatrix[%d] values: [%d, %d]\n", y, LineMatrix[y][0], LineMatrix[y][1]);
+                }
             } else {
                 LineMatrix[y][0] = -1;
                 LineMatrix[y][1] = -1;
             }
         }
 
-        printf("DEBUG: About to copy LineMatrix to vla_line\n");
         memcpy(vla_line.LineMatrix, LineMatrix, sizeof(LineMatrix));
-        printf("DEBUG: Matrix copied successfully\n");
 
-        printf("DEBUG: About to perform ioctl call\n");
         if (ioctl(vga_ball_fd, VGA_BALL_WRITE_LINE, &vla_line)) {
             perror("ioctl(VGA_BALL_WRITE_LINE) failed");
             printf("DEBUG: ioctl failed\n");
             break;
         }
-        printf("DEBUG: ioctl successful\n");
 
         // Small delay to control animation speed
         usleep(SLEEP_TIME);
